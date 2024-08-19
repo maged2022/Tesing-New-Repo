8361740da5e29b229e3d4e25dabe5388dd47fb54
maged mohamed <maged12345@Github.com>
2024-08-19 13:02:03 +0300

    Initial Commit

---
+++ Tesing New Repo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tesing New Repo
+//
+//  Created by maged on 19/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tesing New Repo/Tesing_New_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesing_New_RepoApp.swift
+//  Tesing New Repo
+//
+//  Created by maged on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tesing_New_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
